ab85041d422e6bb9f53e47109dd10dd5edeb8676
Bobby Schulz <schu3119@umn.edu>
2018-07-25 16:20:00 -0500

    Added maxbotix and modified Downhole_BME280
    Downhole_BME280 is ready for deployment

--- Margay/Maxbotics_BME280/Maxbotics_BME280.ino
+++ Margay/Maxbotics_BME280/Maxbotics_BME280.ino
@@ -1,37 +1,36 @@
 //MargayDemo.ino
 #include "Margay.h"
-#include <TP_Downhole.h>
 #include <BME.h>
+#include <Maxbotix.h>
 
-// #define MARGAY_1v0
-TP_Downhole DH; //Initalize TP-Downhole sensor
-BME RH; //Initialzie BME280
+BME RH;
+Maxbotix Range;
 
 String Header = ""; //Information header
-uint8_t I2CVals[3] = {0x6A, 0x77, 0x76}; 
-// int Count = 0;
+uint8_t I2CVals[1] = {0x77}; 
+
 unsigned long UpdateRate = 60; //Number of seconds between readings 
 
 Margay Logger(Model_1v0);
 
 void setup() {
-	Header = Header + DH.GetHeader() + RH.GetHeader();
+	Header = Header + Range.GetHeader() + RH.GetHeader();
 	Logger.begin(I2CVals, sizeof(I2CVals), Header); //Pass header info to logger
 	Init();
 }
 
 void loop() {
+	Init();
 	Logger.Run(Update, UpdateRate);
 }
 
 String Update() 
 {
-	Init();
-	return DH.GetString() + RH.GetString();
+	return Range.GetString() + RH.GetString();
 }
 
 void Init() 
 {
-	DH.begin(TP2A2);
-	RH.begin(0x77);
+	RH.begin();
+	Range.begin(Logger.ExtInt); //Begin maxbotics on cs pin
 }
---
+++ Margay/TP_Downhole/TP_Downhole.ino
@@ -0,0 +1,34 @@
+//MargayDemo.ino
+#include "Margay.h"
+#include <TP_Downhole.h>
+
+TP_Downhole Sensor; //Initalize TP-Downhole sensor
+
+String Header = "Pressure [mBar],Temp DH [C], Temp DHt [C]"; //Information header
+uint8_t I2CVals[2] = {0x6A, 0x76}; 
+unsigned long UpdateRate = 60; //Number of seconds between readings 
+
+Margay Logger(Model_1v0);
+
+void setup() {
+  Logger.begin(I2CVals, sizeof(I2CVals), Header); //Pass header info to logger
+  Init();
+}
+
+void loop() {
+	Init();
+	Logger.Run(Update, UpdateRate);
+}
+
+String Update() 
+{
+	float Val1 = Sensor.getPressure();
+	float Val2 = Sensor.getTemperature(0);
+	float Val3 = Sensor.getTemperature(1);
+	return String(Val1) + "," + String(Val2) + "," + String(Val3); 
+}
+
+void Init() 
+{
+	Sensor.begin(TP2v2);
+}
