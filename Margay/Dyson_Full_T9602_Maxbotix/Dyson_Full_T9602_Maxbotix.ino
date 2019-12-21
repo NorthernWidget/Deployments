a5166225be1bea70285f7d63ff9300d7579c0b3b
Bobby Schulz <schu3119@umn.edu>
2019-12-20 20:11:17 -0600

    Updated Dyson/T9602/Maxbotix file
    Updated Maxbotix interface setup

--- Margay/Dyson_Full_T9602_Maxbotix/Dyson_Full_T9602_Maxbotix.ino
+++ Margay/Dyson_Full_T9602_Maxbotix/Dyson_Full_T9602_Maxbotix.ino
@@ -4,17 +4,17 @@
 #include <DysonLW.h>
 #include <Maxbotix.h>
 
-Margay Logger(Model_2v0, Build_C);
-
 DysonSW PyroUp(UP); //Initialzie Upward facing Dyson short wave
 DysonSW  PyroDown(DOWN); //Initialize Downward facing Dyson short wave
 T9602 RH;  //Initialize T9602 Humidity sensor
 DysonLW Pyrg; //Initialize (Downward) Dyson long wave 
-Maxbotix Range(Logger.D0); // Initialize Maxbotix rangefinder on D0 pin with digital connect
+Maxbotix Range; // Initialize Maxbotix rangefinder
 
 String Header = ""; //Information header
 uint8_t I2CVals[6] = {0x4A, 0x41, 0x53, 0x40, 0x1D, 0x28}; 
 unsigned long UpdateRate = 60; //Number of seconds between readings 
+
+Margay Logger(Model_2v0, Build_C);
 
 void setup() {
   Header = Header + Range.GetHeader() + RH.GetHeader() + Pyrg.GetHeader() + PyroUp.GetHeader() + PyroDown.GetHeader();
@@ -38,5 +38,5 @@ void Init()
   PyroDown.begin();
   Pyrg.begin();
     RH.begin();
-    Range.begin(); //Begin maxbotics on cs pin
+    Range.begin(Logger.RX); //Begin maxbotics on cs pin
 }
